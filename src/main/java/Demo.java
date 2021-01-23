ae5f0d53520bebfd08722de43b038d3148316a24
shuailong <1534068559@qq.com>
2021-01-23 09:52:07 +0800

    JSD2012_SE课上练习

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,2 +1,15 @@
 public class Demo {
+    public static void main(String[] args) {
+        /**
+         * 二进制：
+         *
+         */
+        int n = 50;
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println(n);//
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Integer.toBinaryString(i));
+        }
+    }
 }
